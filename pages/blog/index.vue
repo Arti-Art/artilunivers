8fc4e4b6f3437baa2d78979179816dcef8dd333d
Arti <art.kouznetsov@gmail.com>
2021-05-14 23:33:32 +0200

    First commit
    Finished lecture 15: Adding links & navigating around

---
+++ pages/blog/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    <h1>Blog Page</h1>
+    <input v-model="postId" type="text" placeholder="enter a post ID" />
+    <button @click="onLoadPost">Load Post</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      postId: '',
+    }
+  },
+  methods: {
+    onLoadPost() {
+      this.$router.push(`/blog/${this.postId}`)
+    },
+  },
+})
+</script>
